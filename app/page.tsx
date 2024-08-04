f899bf047439e66b5cf4eb51bc0de5462edb96d7
David Lee <Dai180293@gmail.com>
2024-08-04 12:41:58 +0100

    APPEND: All text and image content added, including the import for the onboarding form (PatientForm.tsx)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,40 @@
+import Image from "next/image";
+import PatientForm from "@/components/forms/PatientForm";
+import Link from "next/link";
+
 export default function Home() {
     return (
-        <section>
-            <h1 className="text-white">Home</h1>
+        <section className="flex h-screen max-h-screen">
+            {/* TODO: OTP | Passkey Verification modal */}
+            <section className="remove-scrollbar container my-auto">
+                <aside className="sub-container max-w-[496px]">
+                    <figure>
+                        <Image
+                            src="/assets/icons/logo-full.svg"
+                            alt="patient"
+                            width={1000}
+                            height={1000}
+                            className="h-10 w-fit"
+                        />
+                    </figure>
+                    <PatientForm />
+                    <div className="text-14-regular flex justify-between">
+                        <p>© 2024 MyDoctor</p>
+                        <Link href="/?admin=true" className="underline text-green-500">
+                            Admin Login
+                        </Link>
+                    </div>
+                </aside>
+            </section>
+            <figure className="max-w-[50%]">
+                <Image
+                    src="/assets/images/onboarding-img.png"
+                    alt="patient"
+                    width={1000}
+                    height={1000}
+                    className="side-img h-full"
+                />
+            </figure>
         </section>
     );
 }
